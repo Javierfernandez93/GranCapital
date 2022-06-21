b925f30d2158c5868bb1519f562f73ebf8aa0c3a
Javier Fernández <leqjl93@hotmail.com>
2022-06-21 18:28:50 -0500

    Fixing styles
    Fixing styles

--- apps/backoffice/view/profile.view.php
+++ apps/backoffice/view/profile.view.php
@@ -1,7 +1,7 @@
 <div id="app">
     <div class="container-fluid">
         <div class="page-header min-height-300 border-radius-xl mt-4"
-            style="background-image: url('../../src/img/curved-images/curved0.jpg'); background-position-y: 50%;">
+            style="background-image: url('../../src/img/trading.webp'); background-position-y: 50%;">
             <span class="mask bg-gradient-primary opacity-6"></span>
         </div>
         <div class="card card-body blur shadow-blur mx-4 mt-n6 overflow-hidden">
