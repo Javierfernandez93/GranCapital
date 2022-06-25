c47e1470f5f89cc617d8477ee09137d38202a8bd
Javier Fernández <leqjl93@hotmail.com>
2022-06-25 00:34:42 -0500

    Trading
    Trading

--- apps/trading/view/index.view.php
+++ apps/trading/view/index.view.php
@@ -1,6 +1,8 @@
 <div class="container-fluid py-4" id="app">
     <!-- <iframe class="vh-100" src="https://trade.mql5.com/trade?servers=SomeBroker1-Demo,SomeBroker1-Live,SomeBroker2-Demo,SomeBroker2-Live&trade_server=SomeBroker-Demo&demo_all_servers=1&startup_version=5&lang=es&save_password=off" allowfullscreen="allowfullscreen" style="width: 100%; height: 100%; border: none;"></iframe> -->
-    <iframe class="vh-100" src="https://trade.mql5.com/trade?startup_version=5&lang=es" allowfullscreen="allowfullscreen" style="width: 100%; height: 100%; border: none;"></iframe>
+    <!-- <iframe class="vh-100" src="https://trade.mql5.com/trade?startup_version=5&lang=es" allowfullscreen="allowfullscreen" style="width: 100%; height: 100%; border: none;"></iframe> -->
+
+        <a href="https://www.myfxbook.com/members/heliolagunae/helio-laguna/9536807"><img alt="widget" src="https://widgets.myfxbook.com/widgets/9536807/small.jpg"/></a>
 
     <!-- 5004322812
     kvduend7 -->
