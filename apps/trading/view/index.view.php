adda373e5c1282b179c43f14ff71062cbe3ca930
Javier Fernández <leqjl93@hotmail.com>
2022-06-25 00:48:56 -0500

    Update index.view.php

--- apps/trading/view/index.view.php
+++ apps/trading/view/index.view.php
@@ -1,26 +1,30 @@
 <div class="container-fluid py-4" id="app">
-    <!-- <iframe class="vh-100" src="https://trade.mql5.com/trade?servers=SomeBroker1-Demo,SomeBroker1-Live,SomeBroker2-Demo,SomeBroker2-Live&trade_server=SomeBroker-Demo&demo_all_servers=1&startup_version=5&lang=es&save_password=off" allowfullscreen="allowfullscreen" style="width: 100%; height: 100%; border: none;"></iframe> -->
-    <!-- <iframe class="vh-100" src="https://trade.mql5.com/trade?startup_version=5&lang=es" allowfullscreen="allowfullscreen" style="width: 100%; height: 100%; border: none;"></iframe> -->
+    <div class="row justify-content-center">
+        <div class="col-8">
+            <!-- <iframe class="vh-100" src="https://trade.mql5.com/trade?servers=SomeBroker1-Demo,SomeBroker1-Live,SomeBroker2-Demo,SomeBroker2-Live&trade_server=SomeBroker-Demo&demo_all_servers=1&startup_version=5&lang=es&save_password=off" allowfullscreen="allowfullscreen" style="width: 100%; height: 100%; border: none;"></iframe> -->
+            <!-- <iframe class="vh-100" src="https://trade.mql5.com/trade?startup_version=5&lang=es" allowfullscreen="allowfullscreen" style="width: 100%; height: 100%; border: none;"></iframe> -->
 
-        <!-- myfxbook.com open trades widget - Start -->
+                <!-- myfxbook.com open trades widget - Start -->
 
-        <a href="https://www.myfxbook.com/members/heliolagunae/helio-laguna/9536807"><img alt="widget" src="https://widgets.myfxbook.com/systems-custom-widget?uid=584467&width=400&height=250&title=Systems&titles=12&showAllSystems=true&bgColor=ffffff&gridColor=bdbdbd&fontColor=525252&chartbgc=ffffff"/></a>
-    <div class="row">
-        <div class="col">
-            <div>
-                <script class="powered" type="text/javascript" src="https://widgets.myfxbook.com/scripts/fxOpenTrades.js?oid=9536807&link=1"></script>
+            <a href="https://www.myfxbook.com/members/heliolagunae/helio-laguna/9536807"><img alt="widget" src="https://widgets.myfxbook.com/systems-custom-widget?uid=584467&width=400&height=250&title=Systems&titles=12&showAllSystems=true&bgColor=ffffff&gridColor=bdbdbd&fontColor=525252&chartbgc=ffffff" class="w-100 mb-3"/></a>
+            <div class="row">
+                <div class="col">
+                    <div>
+                        <script class="powered" type="text/javascript" src="https://widgets.myfxbook.com/scripts/fxOpenTrades.js?oid=9536807&link=1"></script>
+                    </div>
+                    <div style="font-size: 10px">
+                        © Powered by <a href='https://www.myfxbook.com' class='myfxbookLink' target='_self' rel='noopener nofollow'><strong>Myfxbook.com</strong></a>
+                    </div>
+                    <script type="text/javascript">showOpenTradesWidget()</script>
+                </div>
+                <div class="col-auto">
+                    <a href="https://www.myfxbook.com/members/heliolagunae/helio-laguna/9536807"><img alt="widget" src="https://widgets.myfxbook.com/widgets/9536807/small.jpg"/></a>
+                </div>
             </div>
-            <div style="font-size: 10px">
-                © Powered by <a href='https://www.myfxbook.com' class='myfxbookLink' target='_self' rel='noopener nofollow'><strong>Myfxbook.com</strong></a>
-            </div>
-            <script type="text/javascript">showOpenTradesWidget()</script>
-        </div>
-        <div class="col-auto">
-            <a href="https://www.myfxbook.com/members/heliolagunae/helio-laguna/9536807"><img alt="widget" src="https://widgets.myfxbook.com/widgets/9536807/small.jpg"/></a>
+            <!-- myfxbook.com open trades widget - End -->
+
+            <!-- 5004322812
+            kvduend7 -->
         </div>
     </div>
-    <!-- myfxbook.com open trades widget - End -->
-
-    <!-- 5004322812
-    kvduend7 -->
 </div>
