4834205b7c97ab6ddb9444a93dec30f0d0655d8e
Javier Fernández <leqjl93@hotmail.com>
2022-06-30 10:01:04 -0500

    Working on trading view
    Responsive

--- apps/trading/view/index.view.php
+++ apps/trading/view/index.view.php
@@ -1,19 +1,19 @@
 <div class="container-fluid py-4" id="app">
     <div class="row justify-content-center text-center">
-        <div class="col-10">
+        <div class="col-12 col-xl-10">
             <div class="card mb-3">
                 <div class="card-header">
                     <h3>Ganancias del día</h3>
                 </div>
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-4">
+                        <div class="col-12 col-xl-4 mb-3 mb-xl-0">
                             <a href="https://www.myfxbook.com/members/heliolagunae/helio-laguna/9536807"><img alt="widget" src="https://widgets.myfxbook.com/widgets/9536807/medium.jpg"/></a>
                         </div>
-                        <div class="col-4">
+                        <div class="col-12 col-xl-4 mb-3 mb-xl-0">
                             <a href="https://www.myfxbook.com/members/heliolagunae/gran-capital-arbitraje/9606953"><img alt="widget" src="https://widgets.myfxbook.com/widgets/9606953/medium.jpg"/></a>
                         </div>
-                        <div class="col-4">
+                        <div class="col-12 col-xl-4 mb-3 mb-xl-0">
                             <a href="https://www.myfxbook.com/members/heliolagunae/gran-capital-2/9606958"><img alt="widget" src="https://widgets.myfxbook.com/widgets/9606958/medium.jpg"/></a>
                         </div>
                     </div>
@@ -22,13 +22,13 @@
             <div class="card mb-3">
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-4">
+                        <div class="col-12 col-xl-4 mb-3 mb-xl-0">
                             <a href="https://www.myfxbook.com/members/heliolagunae/helio-laguna/9536807"><img class="img-fluid" alt="widget" src="https://widgets.myfxbook.com/custom-widget?id=9536807&width=400&height=250&bart=0&linet=0&symbol=XAUUSD&magic=-1&title=helio laguna&titles=12&bgColor=ffffff&gridColor=bdbdbd&lineColor=ff0505&barColor=ffadad&bar1Color=ffadad&fontColor=525252&chartbgc=ffffff&equityColor=eff45a"/></a>
                         </div>
-                        <div class="col-4">
+                        <div class="col-12 col-xl-4 mb-3 mb-xl-0">
                             <a href="https://www.myfxbook.com/members/heliolagunae/gran-capital-arbitraje/9606953"><img class="img-fluid" alt="widget" src="https://widgets.myfxbook.com/custom-widget?id=9606953&width=400&height=250&bart=0&linet=0&symbol=XAUUSD&magic=-1&title=GranCapitalArbitraje&titles=12&bgColor=ffffff&gridColor=bdbdbd&lineColor=ff0505&barColor=ffadad&bar1Color=ffadad&fontColor=525252&chartbgc=ffffff&equityColor=eff45a"/></a>
                         </div>
-                        <div class="col-4">
+                        <div class="col-12 col-xl-4 mb-3 mb-xl-0">
                             <a href="https://www.myfxbook.com/members/heliolagunae/gran-capital-2/9606958"><img class="img-fluid" alt="widget" src="https://widgets.myfxbook.com/custom-widget?id=9606958&width=400&height=250&bart=0&linet=0&symbol=XAUUSD&magic=-1&title=GranCapital2&titles=12&bgColor=ffffff&gridColor=bdbdbd&lineColor=ff0505&barColor=ffadad&bar1Color=ffadad&fontColor=525252&chartbgc=ffffff&equityColor=eff45a"/></a>
                         </div>
                     </div>
@@ -41,7 +41,7 @@
                 </div>
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-4">
+                        <div class="col-12 col-xl-4 mb-3 mb-xl-0">
                             <!-- myfxbook.com open trades widget - Start -->
                             <div>
                                 <script class="powered" type="text/javascript"
@@ -53,7 +53,7 @@
                             <script type="text/javascript">showOpenTradesWidget()</script>
                             <!-- myfxbook.com open trades widget - End -->
                         </div>
-                        <div class="col-4">
+                        <div class="col-12 col-xl-4 mb-3 mb-xl-0">
                             <!-- myfxbook.com open trades widget - Start -->
                             <div>
                                 <script class="powered" type="text/javascript"
@@ -65,7 +65,7 @@
                             <script type="text/javascript">showOpenTradesWidget()</script>
                             <!-- myfxbook.com open trades widget - End -->
                         </div>
-                        <div class="col-4">
+                        <div class="col-12 col-xl-4 mb-3 mb-xl-0">
                             <!-- myfxbook.com open trades widget - Start -->
                             <div>
                                 <script class="powered" type="text/javascript"
