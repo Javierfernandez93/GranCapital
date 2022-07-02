c409195be278c194a79b6e5500134e0964c81c7c
Javier Fernández <leqjl93@hotmail.com>
2022-07-02 17:57:51 -0500

    Clearing cache
    Clearing cache

--- layout/two_columns.layout.php
+++ layout/two_columns.layout.php
@@ -17,7 +17,7 @@
         <link href="../../src/css/nucleo-svg.css" rel="stylesheet" />
         <link href="../../src/css/animate.css" rel="stylesheet" />
         
-        <link href="../../src/css/general.css" rel="stylesheet" type="text/css">
+        <link href="../../src/css/general.css?t=1" rel="stylesheet" type="text/css">
         <link id="pagestyle" href="../../src/css/soft-ui-dashboard.css?v=1.0.6" rel="stylesheet" />
         %css_scripts%
     </head>
@@ -32,7 +32,7 @@
         %content%
         
         <script src="../../src/js/jquery-3.1.1.js" type="text/javascript"></script>
-        <script src="../../src/js/general.js" type="text/javascript"></script>
+        <script src="../../src/js/general.js?t=1" type="text/javascript"></script>
         
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
         <script src="https://unpkg.com/vue@3"></script>
