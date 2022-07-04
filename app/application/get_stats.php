3aeb68ca958bbc715e8d55ad284ac98912903115
Javier Fernández <leqjl93@hotmail.com>
2022-07-04 14:41:57 -0500

    fixing array
    fixing array

--- app/application/get_stats.php
+++ app/application/get_stats.php
@@ -65,11 +65,14 @@ function getBrokersChartData(array &$data = null,CapitalPerBroker $CapitalPerBro
             $capitals = $CapitalPerBroker->getAllPerBroker($broker['broker_id']);
             $gains = $GainPerBroker->getAllGainsPerBroker($broker['broker_id']);
 
-            $total_capital = $capitals ? array_sum($capitals) : 0;
-            $total_gain = $gains ? array_sum($gains) : 0;
+            $gains = $gains ? $gains : [];
+            $capitals = $capitals ? $capitals : [];
 
-            $data['brokers'][$key]['averange']['capital'] = $total_capital / sizeof($capitals);
-            $data['brokers'][$key]['averange']['gain'] = $total_gain / sizeof($gains);
+            $total_capital = array_sum($capitals);
+            $total_gain = array_sum($gains);
+
+            $data['brokers'][$key]['averange']['capital'] = sizeof($capitals) ? $total_capital / sizeof($capitals) : 0;
+            $data['brokers'][$key]['averange']['gain'] = sizeof($gains) > 0 ? $total_gain / sizeof($gains) : 0;
             $data['pie']['data'][$key] = $total_capital;
         }
 
