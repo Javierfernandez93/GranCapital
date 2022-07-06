438679710c03b7ccbb5d5155e19f9fe85b8af3e8
Javier Fernández <leqjl93@hotmail.com>
2022-07-05 19:52:49 -0500

    fixing cache
    fixing cache

--- src/js/gains.vue.js
+++ src/js/gains.vue.js
@@ -1,4 +1,4 @@
-import { User } from '../../src/js/user.module.js?t=2'
+import { User } from '../../src/js/user.module.js?t=3'
 
 /* vue */
 import { ProfitViewer } from '../../src/js/profitViewer.vue.js?t=1'
