7eca05d5be6161ebd53f9ac93eca3568334ec3dd
Javier Fernández <leqjl93@hotmail.com>
2022-07-15 22:47:08 -0500

    fixing notices
    fixing notices

--- app/application/get_notice.php
+++ app/application/get_notice.php
@@ -14,6 +14,7 @@ if($UserSupport->_loaded === true)
         
         if($notice = $Notice->getNotice($data['notice_id']))
         {
+            format($notice);
             $data["notice"] = $notice;
             $data["s"] = 1;
             $data["r"] = "DATA_OK";
@@ -28,6 +29,16 @@ if($UserSupport->_loaded === true)
 } else {
 	$data["s"] = 0;
 	$data["r"] = "NOT_FIELD_SESSION_DATA";
+}
+
+function format(array &$notice = null)
+{
+    if($notice['start_date'] != 0 && $notice['end_date'] != 0)
+    {
+        $notice['start_date'] = date("Y-m-d",$notice['start_date']);
+        $notice['end_date'] = date("Y-m-d",$notice['end_date']);
+        $notice['limit_dates'] = true;
+    }
 }
 
 echo json_encode($data);
--- app/application/get_notices_list.php
+++ app/application/get_notices_list.php
@@ -10,18 +10,48 @@ if($UserLogin->_loaded === true)
 {
     $Notice = new GranCapital\Notice;
     
-    if($notices = $Notice->getAllPublished())
+    if($notices = format($Notice->getAllPublished()))
     {
         $data["notices"] = $notices;
         $data["s"] = 1;
         $data["r"] = "DATA_OK";
     } else {
         $data["s"] = 0;
-        $data["r"] = "NOT_WALLET";
+        $data["r"] = "NOT_NOTICES";
     }
 } else {
 	$data["s"] = 0;
 	$data["r"] = "NOT_FIELD_SESSION_DATA";
+}
+
+function isAviable(array $notice = null) : bool
+{
+    if($notice['start_date'] == 0 && $notice['end_date'] == 0)
+    {
+        return true;
+    }
+    
+    $time = time();
+
+    return $time >= $notice['start_date'] && $time <= $notice['end_date'];
+}
+
+function format($notices = null)
+{
+    if(is_array($notices) && sizeof($notices) > 0)
+    {
+        foreach($notices as $notice)
+        {
+            if(isAviable($notice))
+            {
+                $_notices[] = $notice;
+            }
+        }
+
+        return $_notices;
+    }
+
+    return false;
 }
 
 echo json_encode($data);
